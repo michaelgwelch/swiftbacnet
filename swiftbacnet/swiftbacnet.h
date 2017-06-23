663a99b918c3ae286ac11b1c74a57ca01bd468a1
Michael Welch <michaelgwelch@gmail.com>
2017-06-22 19:19:11 -0500

    Initial Commit

---
+++ swiftbacnet/swiftbacnet.h
@@ -0,0 +1,19 @@
+//
+//  swiftbacnet.h
+//  swiftbacnet
+//
+//  Created by Michael Welch on 6/22/17.
+//  Copyright © 2017 Michael Welch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for swiftbacnet.
+FOUNDATION_EXPORT double swiftbacnetVersionNumber;
+
+//! Project version string for swiftbacnet.
+FOUNDATION_EXPORT const unsigned char swiftbacnetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftbacnet/PublicHeader.h>
+
+
